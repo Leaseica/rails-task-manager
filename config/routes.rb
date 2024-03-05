0e1fec287b28206a6ab2e1fc7da0a2dc21f229d1
Lea Seica <lea.seica@skema.edu>
2024-03-05 15:40:11 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,20 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  # I can list tasks => All Task #index
+  get '/tasks', to: 'tasks#index'
+
+  # I can add a new task => Create get #new post #create
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+
+  # I can edit a task (mark as completed / update title & details)  => Update get #edit patch #update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :tasks_edit
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # I can remove a task => Remove #destroy
+  delete '/tasks/:id', to: 'tasks#destroy'
+  # I can view the details of a task =>  One task #show
+  get '/tasks/:id', to: 'tasks#show', as: :task
   # root "posts#index"
 end
